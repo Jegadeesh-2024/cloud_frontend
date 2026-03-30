941d132c1bdc641b0e32a6a055ed590f749573cd
Jegadeesh _G _ dev <jegadeeshvijay75@gmail.com>
2026-03-30 14:08:46 +0530

    logiin iinput tet color changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
@@ -6,10 +5,9 @@ import { toast } from "react-toastify";
 import bg from "../assets/bg.jpg";
 
 const Login = () => {
-
   const navigate = useNavigate();
   const API = import.meta.env.VITE_API_URL;
-  const AUTH_API=import.meta.env.VITE_AUTH_API_URL;
+  const AUTH_API = import.meta.env.VITE_AUTH_API_URL;
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
@@ -17,7 +15,7 @@ const Login = () => {
 
     if (token) {
       localStorage.setItem("token", token);
-      
+
       navigate("/folder");
     }
   }, []);
@@ -47,12 +45,9 @@ const Login = () => {
     if (Object.keys(newErrors).length > 0) return;
 
     try {
-      setLoading(true)
+      setLoading(true);
 
-      const res = await axios.post(
-        `${API}/login`,
-        form
-      );
+      const res = await axios.post(`${API}/login`, form);
 
       if (res.data.success) {
         localStorage.setItem("token", res.data.token);
@@ -61,7 +56,6 @@ const Login = () => {
       } else {
         toast.error(res.data.message);
       }
-
     } catch (error) {
       console.log(error);
       toast.error("Server Error");
@@ -69,27 +63,24 @@ const Login = () => {
   };
 
   return (
-
     <div
       className="min-h-screen flex items-center justify-center bg-cover bg-center px-3 md:px-0"
       style={{ backgroundImage: `url(${bg})` }}
     >
-
       <div className="bg-black/40 backdrop-blur-md p-5 md:p-8 rounded-2xl shadow-2xl w-full max-w-md">
-
         <h2 className="text-2xl md:text-3xl font-bold text-white text-center mb-6">
           Login
         </h2>
 
         <form onSubmit={handleSubmit}>
-
           <input
             type="text"
             name="email"
             placeholder="Email"
             value={form.email}
             onChange={handleChange}
-          className="w-full p-2 md:p-3 mb-3 rounded-lg text-sm md:text-base ..."/>
+            className="w-full text-white p-2 md:p-3 mb-3 rounded-lg text-sm md:text-base ..."
+          />
 
           {errors.email && (
             <p className="text-red-300 text-xs md:text-sm mb-2">
@@ -103,7 +94,8 @@ const Login = () => {
             placeholder="Password"
             value={form.password}
             onChange={handleChange}
-className="w-full p-2 md:p-3 mb-3 rounded-lg text-sm md:text-base ..."          />
+            className="w-full text-white p-2 md:p-3 mb-3 rounded-lg text-sm md:text-base ..."
+          />
 
           {errors.password && (
             <p className="text-red-300 text-xs md:text-sm mb-2">
@@ -117,24 +109,24 @@ className="w-full p-2 md:p-3 mb-3 rounded-lg text-sm md:text-base ..."          
             Login
           </button> */}
           <button
-  type="submit"
-  disabled={loading}
-className="w-full bg-green-500 hover:bg-green-600 text-white py-2 rounded-lg mt-2 text-sm md:text-base">
-  {loading ? (
-    <div className="flex items-center gap-2">
-      <div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
-      <span>Logging in...</span>
-    </div>
-  ) : (
-    "Login"
-  )}
-</button>
-
+            type="submit"
+            disabled={loading}
+            className="w-full bg-green-500 hover:bg-green-600 text-white py-2 rounded-lg mt-2 text-sm md:text-base"
+          >
+            {loading ? (
+              <div className="flex items-center gap-2">
+                <div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
+                <span>Logging in...</span>
+              </div>
+            ) : (
+              "Login"
+            )}
+          </button>
         </form>
         <p className="text-white text-center mt-3">OR</p>
 
         <button
-          onClick={()=>window.open(`${AUTH_API}/google`,"_self")}
+          onClick={() => window.open(`${AUTH_API}/google`, "_self")}
           className="w-full mt-3 bg-red-500 hover:bg-red-600 text-white py-2 rounded-lg text-sm md:text-base"
         >
           Login with Google
@@ -146,13 +138,9 @@ className="w-full bg-green-500 hover:bg-green-600 text-white py-2 rounded-lg mt-
             Register
           </Link>
         </p>
-
       </div>
-
     </div>
-
   );
-
 };
 
 export default Login;
